44b4f78e34d63c1d0408c6a00bbfe91027b3d8ab
Asher Dawes <asher.dawes@gmail.com>
2018-08-12 13:26:58 -0700

    Michael f dev (#26)
    * Enable larger transaction lists and improve io
    
    * Fix formatting for address balance displays

--- react/src/components/dashboard/walletsBalance/walletsBalance.render.js
+++ react/src/components/dashboard/walletsBalance/walletsBalance.render.js
@@ -15,7 +15,7 @@ const WalletsBalanceRender = function() {
             this.props.ActiveCoin.coin === 'CHIPS' ||
             (this.props.ActiveCoin.mode === 'spv' && this.props.ActiveCoin.coin !== 'KMD') ||
             this.renderBalance('total') === this.renderBalance('transparent') ||
-            this.renderBalance('total') === 0 ? 'col-lg-12 col-xs-12 balance-placeholder--bold' : 'col-lg-3 col-xs-12'
+            this.renderBalance('total') === 0 ? 'col-lg-12 col-xs-12 balance-placeholder--bold' : 'col-lg-4 col-xs-12'
           }>
             <div className="widget widget-shadow">
               <div className="widget-content">
@@ -31,7 +31,7 @@ const WalletsBalanceRender = function() {
                 }
                 <div className="padding-20 padding-top-10">
                   <div className="clearfix cursor-default">
-                    <div className="pull-left padding-vertical-10">
+                    <div className="pull-left padding-vertical-10 min-width-160l">
                       { this.props.ActiveCoin.coin !== 'CHIPS' &&
                         this.props.ActiveCoin.mode !== 'spv' &&
                         <i className="icon fa-eye font-size-24 vertical-align-bottom margin-right-5"></i>
@@ -54,7 +54,7 @@ const WalletsBalanceRender = function() {
                       }
                     </div>
                     <span
-                      className="pull-right padding-top-10 font-size-22">
+                      className="pull-right padding-top-10 font-size-20 min-width-160r">
                       { this.renderBalance('transparent', true) }
                       <ReactTooltip
                         effect="solid"
@@ -66,16 +66,16 @@ const WalletsBalanceRender = function() {
             </div>
           </div>
 
-          <div className={ (this.props.ActiveCoin.mode === 'native' && Number(this.renderBalance('private'))) > 0 ? 'col-lg-3 col-xs-12' : 'hide' }>
+          <div className={ (this.props.ActiveCoin.mode === 'native' && Number(this.renderBalance('private'))) > 0 ? 'col-lg-4 col-xs-12' : 'hide' }>
             <div className="widget widget-shadow">
               <div className="padding-20 padding-top-10">
                 <div className="clearfix cursor-default">
-                  <div className="pull-left padding-vertical-10">
+                  <div className="pull-left padding-vertical-10 min-width-160l">
                     <i className="icon fa-eye-slash font-size-24 vertical-align-bottom margin-right-5"></i>
                     { translate('INDEX.Z_BALANCE') }
                   </div>
                   <span
-                    className="pull-right padding-top-10 font-size-22"
+                    className="pull-right padding-top-10 font-size-20 min-width-160r"
                     data-tip={ Config.roundValues ? this.renderBalance('private') : '' }>
                     { this.renderBalance('private', true) }
                   </span>
@@ -87,17 +87,17 @@ const WalletsBalanceRender = function() {
             </div>
           </div>
 
-          <div className={ this.props.ActiveCoin.coin === 'KMD' && Number(this.renderBalance('interest')) > 0 ? 'col-lg-3 col-xs-12' : 'hide' }>
+          <div className={ this.props.ActiveCoin.coin === 'KMD' && Number(this.renderBalance('interest')) > 0 ? 'col-lg-4 col-xs-12' : 'hide' }>
             <div className="widget widget-shadow">
               <div className="widget-content">
                 <div className="padding-20 padding-top-10">
                   <div className="clearfix cursor-default">
-                    <div className="pull-left padding-vertical-10">
+                    <div className="pull-left padding-vertical-10 min-width-160l">
                       <i className="icon fa-money font-size-24 vertical-align-bottom margin-right-5"></i>
                       { translate('INDEX.INTEREST_EARNED') }
                     </div>
                     <span
-                      className="pull-right padding-top-10 font-size-22"
+                      className="pull-right padding-top-10 font-size-20 min-width-160r"
                       data-tip={ Config.roundValues ? this.renderBalance('interest') : '' }>
                       { this.renderBalance('interest', true) }
                     </span>
@@ -114,18 +114,18 @@ const WalletsBalanceRender = function() {
             this.props.ActiveCoin.coin === 'CHIPS' ||
             (this.props.ActiveCoin.coin !== 'KMD' && this.props.ActiveCoin.mode === 'spv') ||
             Number(this.renderBalance('total')) === 0 ||
-            this.renderBalance('total') === this.renderBalance('transparent') ? 'hide' : 'col-lg-3 col-xs-12'
+            this.renderBalance('total') === this.renderBalance('transparent') ? 'hide' : 'col-lg-4 col-xs-12'
           }>
             <div className="widget widget-shadow">
               <div className="widget-content">
                 <div className="padding-20 padding-top-10">
                   <div className="clearfix cursor-default">
-                    <div className="pull-left padding-vertical-10">
+                    <div className="pull-left padding-vertical-10 min-width-160l">
                       <i className="icon fa-bullseye font-size-24 vertical-align-bottom margin-right-5"></i>
                       { translate('INDEX.TOTAL_BALANCE') }
                     </div>
                     <span
-                      className="pull-right padding-top-10 font-size-22"
+                      className="pull-right padding-top-10 font-size-20 min-width-160r"
                       data-tip={ Config.roundValues ? this.renderBalance('total') : '' }>
                       { this.renderBalance('total', true) }
                     </span>
