da1f83251cabbc66188ba137b41edcb3ed8bc0c5
pbca26 <pbca26@gmail.com>
2019-06-08 21:23:38 +0300

    drop malicious package (https://blog.npmjs.org/post/185397814280/plot-to-steal-cryptocurrency-foiled-by-the-npm)

--- react/src/reducers/toaster.js
+++ react/src/reducers/toaster.js
@@ -2,8 +2,6 @@ import {
   ADD_TOASTER_MESSAGE,
   REMOVE_TOASTER_MESSAGE,
 } from '../actions/storeType';
-import config from '../config';
-import notify from 'electron-native-notify';
 
 const arrayToString = (arr) => {
   if (typeof arr === 'object') {
@@ -20,18 +18,13 @@ export const toaster = (state = {
 
   switch (action.type) {
     case ADD_TOASTER_MESSAGE:
-      if(config.notifications && notify.isSupported()) {
-	notify(action.title, action.message.join ? action.message.join('\n') : action.message);
-        return state;
-      }
-
       let _isSameToastTwice = false;
 
       for (let i = 0; i < state.toasts.length; i++) {
         if (state.toasts[i].title === action.title &&
             (arrayToString(action.message) === arrayToString(state.toasts[i].message) ||
             state.toasts[i].message === action.message) &&
-            state.toasts[i]['_type'] === action['_type']) {
+            state.toasts[i]._type === action._type) {
           _isSameToastTwice = true;
           break;
         }
