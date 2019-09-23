130c00c7c5d450d72ad8a637c658bae9e3a7577b
michaeltout <toutonghi.michael@gmail.com>
2019-09-23 18:11:51 +0200

    Fix bug where estimated block rewards were wrong for chain

--- react/src/util/pbaas/pbaasTxUtils.js
+++ react/src/util/pbaas/pbaasTxUtils.js
@@ -16,8 +16,14 @@ export const estimateReward = (chainDefinition, lastconfirmedheight) => {
 
   let currentEra = eras[eraIndex]
 
-  if (eras.length === 1 && currentEra.eraend === 0 && currentEra.decay === 0 && currentEra.halving === 0) {
-    //If there is no decay or halving, always return current reward, as it doesn't change
+  if ((eras.length === 1 && 
+      currentEra.eraend === 0 && 
+      currentEra.decay === 0) || 
+      (currentEra.decay === LINEAR_DECAY && 
+      Number(currentEra.eraend) === 0) || 
+      currentEra.halving === 0) {
+    //If there is no decay or halving, or linear decay without end, 
+    //always return current reward, as it doesn't change
     reward = fromSats(currentEra.reward)
   } else if (Number(currentEra.decay) === LINEAR_DECAY) {
     //If decay is linear, create y=mx+b line function to estimate reward
