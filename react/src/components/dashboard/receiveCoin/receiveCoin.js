3c2d8fb0d5da4cfba0b2900dc69a67d645ec56e4
michaeltout <toutonghi.michael@gmail.com>
2019-09-25 16:28:39 +0200

    Fix Qr modal not showing in receive screen

--- react/src/components/dashboard/receiveCoin/receiveCoin.js
+++ react/src/components/dashboard/receiveCoin/receiveCoin.js
@@ -56,7 +56,9 @@ class ReceiveCoin extends React.Component {
     this.renderAddressList = this.renderAddressList.bind(this);
   }
 
-  toggleAddressMenu(address) {
+  toggleAddressMenu(address, e) {
+    if (e && e.target && e.target.id === "qrcode-modal-btn") return
+    
     this.setState({
       toggledAddressMenu: this.state.toggledAddressMenu === address ? null : address,
     }, () => {
